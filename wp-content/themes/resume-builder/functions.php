ac41569897de81945945860c3f0e5fa9948d0d1e
IagoGoncalves <iago_375@hotmail.com>
2024-10-22 14:56:20 -0300

    feat: adjust admin

--- wp-content/themes/resume-builder/functions.php
+++ wp-content/themes/resume-builder/functions.php
@@ -69,14 +69,18 @@ function jokerClown() {
     $user = wp_get_current_user();
 
     if ($user->user_login !== WP_UR_U) {
-        remove_menu_page('edit.php');                    
-        remove_menu_page('edit-comments.php');           
-        remove_menu_page('themes.php');                  
-        remove_menu_page('plugins.php');                 
-        remove_menu_page('users.php');                   
-        remove_menu_page('tools.php');                   
-        remove_menu_page('options-general.php');         
+        remove_menu_page('edit.php'); 
+        remove_menu_page('upload.php'); 
+        remove_menu_page('edit.php?post_type=page'); 
+        remove_menu_page('edit-comments.php');
+        remove_menu_page('themes.php');
+        remove_menu_page('plugins.php');
+        remove_menu_page('users.php');
+        remove_menu_page('tools.php');
+        remove_menu_page('options-general.php');
         remove_menu_page('edit.php?post_type=acf-field-group');
+        remove_menu_page('litespeed'); 
+        remove_menu_page('update-core.php');
     }
 }
 add_action('admin_menu', 'jokerClown', 999);
