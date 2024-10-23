e7f6baaface2bd1d8e362d0afc17911a6aa88aea
IagoGoncalves <iago_375@hotmail.com>
2024-10-23 13:42:43 -0300

    feat: alternância de páginas

--- wp-content/themes/resume-builder/archive-data.php
+++ wp-content/themes/resume-builder/archive-data.php
@@ -1,8 +1,10 @@
 <?php
 get_header(); 
 ?>
-<main id="example" class="example" tabindex="-1" role="main">
-    
+<main id="data" class="data" tabindex="-1" role="main">
+    <?php $args = array('post_type' => 'data', 'posts_per_page' => -1); $var = new WP_Query($args); if($var->have_posts()): while($var->have_posts()): $var->the_post();?>																	 
+        <?php the_title(); ?>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
 </main>
 <?php
 get_footer();
--- wp-content/themes/resume-builder/index.php
+++ wp-content/themes/resume-builder/index.php
@@ -5,63 +5,6 @@
  */
 get_header(); ?>
    	<main class="home" id="print-section">
-	   <?php $args = array('post_type' => 'data', 'posts_per_page' => -1); $var = new WP_Query($args); if($var->have_posts()): while($var->have_posts()): $var->the_post();?>																	 
-				<div class="resume-personal-data" id="print-section">
-					<figure><img src="<?= get_field('dados_pessoais_foto_de_perfil');?>" alt="Foto de perfil"></figure>
-					<div class="resume-photo">
-						<p class="nome-title"><?= get_field('dados_pessoais_nome_completo');?></p>
-						<p><?= get_field('dados_pessoais_idade') . ', ' . get_field('dados_pessoais_sexo') . ', ' . get_field('dados_pessoais_estado_civil') . ', ' . get_field('dados_pessoais_nacionalidade');?></p>
-						<p><?= get_field('dados_pessoais_endereco') . ' - ' . get_field('dados_pessoais_cidade') . ' - ' . get_field('dados_pessoais_estado'); ?></p>
-						<p><?= get_field('dados_pessoais_telefone_1');?></p>
-						<p><?= get_field('dados_pessoais_telefone_2');?></p>
-						<a href="<?= get_field('dados_pessoais_email');?>"><?= get_field('dados_pessoais_email');?></a>
-					</div>
-				</div>
-				<div class="resume-goals">
-					<h2 class="title-print">Objetivo</h2>
-					<p><?= get_field('objetivo')?></p>
-				</div>
-				<?php if( have_rows('formacao_academica') ): ?>
-					<div class="resume-academic-education">
-						<h2 class="title-print">Formação acadêmica</h2>
-						<?php while( have_rows('formacao_academica') ): the_row(); ?>
-							<span>
-								<h3><?php the_sub_field('nome_do_curso'); ?></h3>
-								<p><?php the_sub_field('instituicao'); ?></p>
-								<p><?= the_sub_field('nivel_do_curso');?>, <?= the_sub_field('mes_e_ano_da_conclusao'); ?></p>
-							</span>
-						<?php endwhile; ?>
-					</div>
-				<?php endif; ?>
-				<?php if( have_rows('experiencia_profissional') ): ?>
-					<div class="resume-professional-experience">
-						<h2 class="title-print">Experiência profissional</h2>
-						<?php while( have_rows('experiencia_profissional') ): the_row(); ?>
-							<span>
-								<h3><?php the_sub_field('nome_da_empresa'); ?></h3>
-								<p><?= the_sub_field('mes_e_ano_de_inicio');?> - <?= the_sub_field('mes_e_ano_de_saida'); ?></p>
-								<p><?php the_sub_field('principais_atividades_desempenhadas_no_cargo'); ?></p>
-							</span>
-						<?php endwhile; ?>
-					</div>
-				<?php endif; ?>
-				<?php if( have_rows('qualificacoes_e_atividades_complementares') ): ?>
-					<div class="resume-qualify">
-						<h2 class="title-print">Qualificações e atividades complementares</h2>
-						<?php while( have_rows('qualificacoes_e_atividades_complementares') ): the_row(); ?>
-							<p><?php the_sub_field('qualificacao'); ?></p>
-						<?php endwhile; ?>
-					</div>
-				<?php endif; ?>
-				<?php if( have_rows('informacoes_adicionais') ): ?>
-					<div class="resume-aditional-info">
-						<h2 class="title-print">Informações adicionais</h2>
-						<?php while( have_rows('informacoes_adicionais') ): the_row(); ?>
-							<p><?php the_sub_field('informacao'); ?></p>
-						<?php endwhile; ?>
-					</div>
-				<?php endif; ?>
-			</div>
-		<?php endwhile; endif; wp_reset_postdata(); ?>
+	   	<h1>home</h1>
    	</main>
 <?php get_footer(); ?>
